e55f89689278eb5e7639178b904e339c409b9815
Mahmoud <rozlvet66@gmail.com>
2022-11-16 18:39:21 +0200

    heuristics

--- Agent.py
+++ Agent.py
@@ -1,11 +1,11 @@
 """
-Connect-4
+Tic Tac Toe Player
 """
 import copy
 
 import numpy as np
 from scipy.signal import convolve2d
-from Utils.StateNode import State
+import Utils.StateNode
 from Utils.Board import Board
 
 max_level = 42
@@ -13,7 +13,29 @@ red = "red"
 yellow = "yellow"
 EMPTY = ""
 tree_root = None
-depth = 0
+INFINITY = 9223372036854775807
+
+# Heuristics----------------
+board_weights = [
+    [30, 40, 50, 70, 50, 40, 30],
+    [40, 60, 80, 100, 80, 60, 40],
+    [50, 80, 110, 130, 110, 80, 50],
+    [50, 80, 110, 130, 110, 80, 50],
+    [40, 60, 80, 100, 80, 60, 40],
+    [30, 40, 50, 70, 50, 40, 30]
+]
+
+level4_heuristic = 1000000
+level3_heuristic = 50000
+level2_heuristic = 50
+level1_heuristic = 5
+
+RED = 1
+YELLOW = 0
+Empty = -1
+
+
+# ---------------------------
 
 
 def initial_state():
@@ -81,7 +103,7 @@ def get_player_board(board, player_color):
     """
     TODO: UPDATE FOR BITS REPRESENTATION (DONE)
     """
-    # KOTB WORK 
+    # KOTB WORK
     if player_color == red:
         player_color = 1
     elif player_color == yellow:
@@ -131,12 +153,13 @@ def exact_utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    score = get_score(board)
-    if score[0] > score[1]:
+    winner_symbol = winner(board)
+    if winner_symbol == red:
         return 1
-    if score[0] < score[1]:
+    elif winner_symbol == yellow:
         return -1
-    return 0
+    elif winner_symbol is None:
+        return 0
 
 
 def expected_utility(board):
@@ -145,7 +168,389 @@ def expected_utility(board):
     TODO: implement this function
     """
 
-    return 0
+    print("----------------heuristics----------------")
+    boardx = []
+    for i in range(6):
+        dd = []
+        for j in range(7):
+            if board.retrieve(i, j) == 1:
+                dd.append('R')
+            elif board.retrieve(i, j) == 0:
+                dd.append('Y')
+            elif board.retrieve(i, j) == -1:
+                dd.append(' ')
+        boardx.append(dd)
+    print(boardx)
+
+    red_score = get_score(board)[0]
+    yellow_score = get_score(board)[1]
+    if red_score > yellow_score:
+        return RED
+    elif red_score < yellow_score:
+        return -1
+    red_total_value = 0
+    yellow_total_value = 0
+    """CHECK THIRD LEVEL"""
+    # rowbyrow
+    for i in range(5, -1, -1):
+        for j in range(5):
+            sum_of_red = 0
+            sum_of_yellow = 0
+            for k in range(3):
+                if board.retrieve(i, j + k) == RED:
+                    sum_of_red += 1
+                elif board.retrieve(i, j + k) == YELLOW:
+                    sum_of_yellow += 1
+            # -rrr- has 4th level value
+            if sum_of_red == 3:
+                red_total_value += level_three_rowbyrow(board, i, j)
+            elif sum_of_yellow == 3:
+                yellow_total_value += level_three_rowbyrow(board, i, j)
+
+    # colbycol
+    for j in range(7):
+        for i in range(5, 1, -1):
+            sum_of_red = 0
+            sum_of_yellow = 0
+            for k in range(3):
+                if board.retrieve(i - k, j) == RED:
+                    sum_of_red += 1
+                elif board.retrieve(i - k, j) == YELLOW:
+                    sum_of_yellow += 1
+            if sum_of_red == 3:
+                if board.retrieve(i - 3, j) == Empty:
+                    red_total_value += level3_heuristic
+            elif sum_of_yellow == 3:
+                if board.retrieve(i - 3, j) == Empty:
+                    yellow_total_value += level3_heuristic
+
+    # diagonally
+    # first half this direction /
+    for i in range(2, 6):
+        for j in range(-1 + i):
+            sum_of_red = 0
+            sum_of_yellow = 0
+            for k in range(3):
+                if board.retrieve(i - k - j, j + k) == RED:
+                    sum_of_red += 1
+                elif board.retrieve(i - k - j, j + k) == YELLOW:
+                    sum_of_yellow += 1
+            if sum_of_red == 3:
+                red_total_value += level_three_diagonal_smallAngle(board, i, j)
+            if sum_of_yellow == 3:
+                yellow_total_value += level_three_diagonal_smallAngle(board, i, j)
+
+    # second half this direction /
+    for j in range(1, 5):
+        for k in range(5 - j):
+            row = 5 - k
+            row2 = row
+            col = j + k
+            col2 = col
+            sum_of_red = 0
+            sum_of_yellow = 0
+            for i in range(3):
+                if board.retrieve(row, col) == RED:
+                    sum_of_red += 1
+                elif board.retrieve(row, col) == YELLOW:
+                    sum_of_yellow += 1
+                row -= 1
+                col += 1
+            if sum_of_red == 3:
+                red_total_value += level_three_diagonal_smallAngle(board, row2, col2)
+            if sum_of_yellow == 3:
+                yellow_total_value += level_three_diagonal_smallAngle(board, row2, col2)
+
+    # \ this direction upper half
+    for j in range(1, 6):
+        for k in range(5 - j):
+            row = k
+            col = j + k
+            row2 = row
+            col2 = col
+            sum_of_red = 0
+            sum_of_yellow = 0
+            for i in range(3):
+                if board.retrieve(row, col) == RED:
+                    sum_of_red += 1
+                elif board.retrieve(row, col) == YELLOW:
+                    sum_of_yellow += 1
+                row += 1
+                col += 1
+            if sum_of_red == 3:
+                red_total_value += level_three_diagonal_smallAngle(board, row2, col2)
+            if sum_of_yellow == 3:
+                yellow_total_value += level_three_diagonal_smallAngle(board, row2, col2)
+
+    # \ this direction lower half
+    for i in range(4):
+        for k in range(4 - i):
+            row = i + k
+            col = k
+            row2 = row
+            col2 = col
+            sum_of_red = 0
+            sum_of_yellow = 0
+            for j in range(3):
+                if board.retrieve(row, col) == RED:
+                    sum_of_red += 1
+                elif board.retrieve(row, col) == YELLOW:
+                    sum_of_yellow += 1
+                row += 1
+                col += 1
+            if sum_of_red == 3:
+                red_total_value += level_three_diagonal_smallAngle(board, row2, col2)
+            if sum_of_yellow == 3:
+                yellow_total_value += level_three_diagonal_smallAngle(board, row2, col2)
+
+    """CHECK SECOND LEVEL AND FIRST LEVEL"""
+    # rowbyrow
+    for i in range(5, -1, -1):
+        for j in range(6):
+            sum_of_red = 0
+            sum_of_yellow = 0
+            red_locations_sum = 0
+            yellow_locations_sum = 0
+            for k in range(2):
+                if board.retrieve(i, j + k) == RED:
+                    sum_of_red += 1
+                    red_locations_sum += board_weights[i][j + k]
+                elif board.retrieve(i, j + k) == YELLOW:
+                    sum_of_yellow += 1
+                    yellow_locations_sum += board_weights[i][j + k]
+            # -rrr- has 4th level value
+            if sum_of_red == 2:
+                # double
+                red_total_value += red_locations_sum * level2_heuristic
+            elif sum_of_yellow == 3:
+                yellow_total_value += yellow_locations_sum * level2_heuristic
+            else:
+                # single
+                red_total_value += red_locations_sum * level1_heuristic
+                yellow_total_value += yellow_locations_sum * level1_heuristic
+
+    # colbycol
+    for j in range(7):
+        for i in range(5, 0, -1):
+            sum_of_red = 0
+            sum_of_yellow = 0
+            red_locations_sum = 0
+            yellow_locations_sum = 0
+            for k in range(2):
+                if board.retrieve(i - k, j) == RED:
+                    sum_of_red += 1
+                    red_locations_sum += board_weights[i - k][j]
+                elif board.retrieve(i - k, j) == YELLOW:
+                    sum_of_yellow += 1
+                    yellow_locations_sum += board_weights[i - k][j]
+            if sum_of_red == 2:
+                # if one on the top is empty
+                if board.retrieve(i - 2, j) == Empty:
+                    red_total_value += red_locations_sum * level2_heuristic
+            elif sum_of_yellow == 2:
+                if board.retrieve(i - 2, j) == Empty:
+                    yellow_total_value += yellow_locations_sum * level2_heuristic
+            else:
+                # single
+                red_total_value += red_locations_sum * level1_heuristic
+                yellow_total_value += yellow_locations_sum * level1_heuristic
+
+    # diagonally
+    # first half this direction /
+    for i in range(1, 6):
+        for j in range(i):
+            sum_of_red = 0
+            sum_of_yellow = 0
+            red_locations_sum = 0
+            yellow_locations_sum = 0
+            for k in range(2):
+                if board.retrieve(i - k - j, j + k) == RED:
+                    sum_of_red += 1
+                    red_locations_sum += board_weights[i - k - j][j + k]
+                elif board.retrieve(i - k - j, j + k) == YELLOW:
+                    sum_of_yellow += 1
+                    yellow_locations_sum += board_weights[i - k - j][j + k]
+            if sum_of_red == 2:
+                # double
+                red_total_value += red_locations_sum * level2_heuristic
+            elif sum_of_yellow == 3:
+                yellow_total_value += yellow_locations_sum * level2_heuristic
+            else:
+                # single
+                red_total_value += red_locations_sum * level1_heuristic
+                yellow_total_value += yellow_locations_sum * level1_heuristic
+
+    # second half this direction /
+    for j in range(1, 6):
+        for k in range(6 - j):
+            row = 5 - k
+            col = j + k
+            sum_of_red = 0
+            sum_of_yellow = 0
+            red_locations_sum = 0
+            yellow_locations_sum = 0
+            for i in range(2):
+                if board.retrieve(row, col) == RED:
+                    sum_of_red += 1
+                    red_locations_sum += board_weights[row][col]
+                elif board.retrieve(row, col) == YELLOW:
+                    sum_of_yellow += 1
+                    yellow_locations_sum += board_weights[row][col]
+                row -= 1
+                col += 1
+            if sum_of_red == 2:
+                # double
+                red_total_value += red_locations_sum * level2_heuristic
+            elif sum_of_yellow == 3:
+                yellow_total_value += yellow_locations_sum * level2_heuristic
+            else:
+                # single
+                red_total_value += red_locations_sum * level1_heuristic
+                yellow_total_value += yellow_locations_sum * level1_heuristic
+
+    # \ this direction upper half
+    for j in range(1, 7):
+        for k in range(6 - j):
+            row = k
+            col = j + k
+            sum_of_red = 0
+            sum_of_yellow = 0
+            red_locations_sum = 0
+            yellow_locations_sum = 0
+            for i in range(2):
+                if board.retrieve(row, col) == RED:
+                    sum_of_red += 1
+                    red_locations_sum += board_weights[row][col]
+                elif board.retrieve(row, col) == YELLOW:
+                    sum_of_yellow += 1
+                    yellow_locations_sum += board_weights[row][col]
+                row += 1
+                col += 1
+            if sum_of_red == 2:
+                # double
+                red_total_value += red_locations_sum * level2_heuristic
+            elif sum_of_yellow == 3:
+                yellow_total_value += yellow_locations_sum * level2_heuristic
+            else:
+                # single
+                red_total_value += red_locations_sum * level1_heuristic
+                yellow_total_value += yellow_locations_sum * level1_heuristic
+
+    # \ this direction lower half
+    for i in range(5):
+        for k in range(5 - i):
+            row = i + k
+            col = k
+            sum_of_red = 0
+            sum_of_yellow = 0
+            red_locations_sum = 0
+            yellow_locations_sum = 0
+            for j in range(2):
+                if board.retrieve(row, col) == RED:
+                    sum_of_red += 1
+                    red_locations_sum += board_weights[row][col]
+                elif board.retrieve(row, col) == YELLOW:
+                    sum_of_yellow += 1
+                    yellow_locations_sum += board_weights[row][col]
+                row += 1
+                col += 1
+            if sum_of_red == 2:
+                # double
+                red_total_value += red_locations_sum * level2_heuristic
+            elif sum_of_yellow == 3:
+                yellow_total_value += yellow_locations_sum * level2_heuristic
+            else:
+                # single
+                red_total_value += red_locations_sum * level1_heuristic
+                yellow_total_value += yellow_locations_sum * level1_heuristic
+
+    print(f"***\nred total value: {red_total_value} \n yellow total value: {yellow_total_value}")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    return red_total_value - yellow_total_value
+
+
+
+
+def level_three_rowbyrow(board, i, j):
+    sum = 0
+    check1: bool = False
+    check2: bool = False
+    if j - 1 >= 0 and board.retrieve(i, j - 1) == Empty:
+        check1 = True
+    if j + 3 <= 6 and board.retrieve(i, j + 3) == Empty:
+        check2 = True
+    if check1 and check2:
+        sum += level4_heuristic
+        row = i + 1
+        col = j - 1
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+        row = i + 1
+        col = j + 3
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+    elif check1:
+        sum += level3_heuristic
+        row = i + 1
+        col = j - 1
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+    elif check2:
+        sum += level3_heuristic
+        row = i + 1
+        col = j + 3
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+    return sum
+
+
+
+
+
+def level_three_diagonal_smallAngle(board, i, j):
+    check1 = False
+    check2 = False
+    sum = 0
+    if i-1>=0 and j-1>=0 and board.retrieve(i-1, j-1) == Empty:
+        check1 = True
+    if i+3<=5 and j+3<=6 and board.retrieve(i + 3, j + 3) == Empty:
+        check2 = True
+    if check1 and check2:
+        sum += level4_heuristic / 5
+        row = i + 1
+        col = j - 1
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+        row = i + 1
+        col = j + 3
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+    elif check1:
+        sum += level3_heuristic
+        row = i + 1
+        col = j - 1
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+    elif check2:
+        row = i + 1
+        col = j + 3
+        while row < 5 and board.retrieve(row, col) == Empty:
+            sum -= 5000
+            row += 1
+
+    return sum
+
+
+
+
+
 
 
 def limited_terminal(level, max_depth):
@@ -153,14 +558,14 @@ def limited_terminal(level, max_depth):
     Returns True if the game is over, False otherwise.
     note: it works for limited and unlimited search
     """
-    return max_depth == 0 or level == max_depth
+    return max_level == 0 or level == max_depth + 1
 
 
 def unlimited_terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if player(board) is None:
+    if winner(board) is not None or player(board) is None:
         return True
     else:
         return False
@@ -170,25 +575,27 @@ def minimax(board, pruning, limited_depth):
     """
     Returns the optimal action for the current player on the board.
     """
-    root = State(board)
+    print("minmax")
+    root = Utils.StateNode.State(board)
     global depth, tree_root
+
     depth = limited_depth
     if player(board) == red:
-        optimal = (-10, None)
+        optimal = (-INFINITY, None)
         for action in actions(board):
-            child = State(result(board, action))
+            child = Utils.StateNode.State(result(board, action))
             root.add_child(child)
-            utility_value = min_value(child, optimal[0], pruning, 1)
+            utility_value = min_value(child, optimal[0], pruning, 1 if limited_depth < max_level else -1)
             if utility_value > optimal[0]:
                 optimal = (utility_value, action)
             root.set_utility(utility_value)
 
     else:
-        optimal = (10, None)
+        optimal = (INFINITY, None)
         for action in actions(board):
-            child = State(result(board, action))
+            child = Utils.StateNode.State(result(board, action))
             root.add_child(child)
-            utility_value = max_value(child, optimal[0], pruning, 1)
+            utility_value = max_value(child, optimal[0], pruning, 1 if limited_depth < max_level else -1)
             if utility_value < optimal[0]:
                 optimal = (utility_value, action)
             root.set_utility(utility_value)
@@ -197,34 +604,32 @@ def minimax(board, pruning, limited_depth):
     return optimal[1], tree_root
 
 
-def max_value(child, predecessor_v, pruning, level_no):
-    if unlimited_terminal(child.board):
-        return exact_utility(child.board)
-    if limited_terminal(level_no, depth):
+def max_value(child, predecessor_v, pruning, level_no=-1):
+    if level_no != -1 and limited_terminal(level_no, depth):
         return expected_utility(child.board)
-
-    v = -10
+    if level_no == -1 and unlimited_terminal(child.board):
+        return exact_utility(child.board)
+    v = -1*INFINITY
     for action in actions(child.board):
-        ch_child = State(result(child.board, action))
+        ch_child = Utils.StateNode.State(result(child.board, action))
         child.add_child(ch_child)
-        v = max(v, min_value(ch_child, v, pruning, level_no + 1))
+        v = max(v, min_value(ch_child, v, pruning, level_no + 1 if level_no != -1 else -1))
         child.set_utility(v)
         if v > predecessor_v and pruning:
             break
     return v
 
 
-def min_value(child, predecessor_v, pruning, level_no):
-    if unlimited_terminal(child.board):
-        return exact_utility(child.board)
-    if limited_terminal(level_no, depth):
+def min_value(child, predecessor_v, pruning, level_no=-1):
+    if level_no != -1 and limited_terminal(level_no, depth):
         return expected_utility(child.board)
-
-    v = 10
+    if level_no == -1 and unlimited_terminal(child.board):
+        return exact_utility(child.board)
+    v = INFINITY
     for action in actions(child.board):
-        ch_child = State(result(child.board, action))
+        ch_child = Utils.StateNode.State(result(child.board, action))
         child.add_child(ch_child)
-        v = min(v, max_value(ch_child, v, pruning, level_no + 1))
+        v = min(v, max_value(ch_child, v, pruning, level_no + 1 if level_no != -1 else -1))
         child.set_utility(v)
         if v < predecessor_v and pruning:
             break
